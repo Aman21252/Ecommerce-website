159ddf6d1d887b5eec8f42c28e57fc1b986f0fae
Shaik <shaikamanulla3424@gmail.com>
2024-06-18 17:02:09 -0400

    Added sub.java

---
+++ sub.java
@@ -0,0 +1,14 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        // Define three variables
+        int a = 10;
+        int b = 5;
+        int c = 2;
+
+        // Perform the subtraction
+        int result = a - b - c;
+
+        // Print the result
+        System.out.println("The result of " + a + " - " + b + " - " + c +" is: " + result);
+    }
+}
